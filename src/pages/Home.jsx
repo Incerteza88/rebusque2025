8b8cd17ebc61847a95cb15d6c9ad1f526fc5e91a
Incerteza88 <info@incerteza.es>
2025-08-18 17:17:23 +0200

    Inicio de registro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import '../styles/index.css'
-import useGlobalReducer from '../hooks/useGlobalReducer'
+import React, { useEffect, useState } from "react"
+import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { RegisterForm } from "../components/RegisterForm.jsx";
+import coffeman from "../assets/img/hombrePC.jpg"
+import { useNavigate, useParams } from "react-router-dom";
 
-function App() {
-  const { store, dispatch } = useGlobalReducer()
-  console.log(store.prueba)
-  return (
-    <>
-      <h1 className='prueba text-success'>Prueba</h1>
-      <button className='btn btn-success' onClick={() => dispatch({ type: "prueba", payload: "hola" })}></button>
-    </>
-  )
-}
+export const Home = () => {
 
-export default App
+	const params = useParams()
+	const navigate = useNavigate()
+	useEffect(()=>{
+		if (params.type != "login" && params.type != "signup") {
+			navigate("/")
+		}
+	}, [])
+
+	return (
+		<>
+			<div className="container">
+
+				<div className="row">
+					<section className="col-6 d-flex justify-content-center align-items-center flex-column">
+						<div className="col-12 ps-3">
+							<h1 className="text-start mb-3">{params.type == "login" ? "Login" : "Sign Up"}</h1>
+							<h6 className="text-start mb-3">Join top freelancers on Freelance</h6>
+						</div>
+						<RegisterForm isLoginType = {params.type == "login"}/>
+					</section>
+
+					<section className="col-6">
+						<img style={{ width: 500, height: "auto" }} src={coffeman} />
+					</section>
+
+				</div>
+
+			</div>
+		</>
+	);
+}; 
