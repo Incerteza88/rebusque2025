87d0108e81e04890c265389c4d58fbd2813d888f
Incerteza88 <info@incerteza.es>
2025-08-21 16:21:27 +0200

    acabando registerform de azul y con el nuevo muckup

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -22,7 +22,9 @@ export const RegisterForm = ({ isLoginType }) => {
       })
     }
     setInputs({
-      fullName: '',
+      nombre: '',
+      apellidos: '',
+      telefono: '',
       email: '',
       password: ''
     })
@@ -36,46 +38,89 @@ export const RegisterForm = ({ isLoginType }) => {
   return (
     <div className='container'>
 
-      <form className=' me-5' onSubmit={sendData}>
-        {!isLoginType && (<div className="mb-4 col-8">
-          <label for="exampleInputEmail1" className="form-label">Full Name</label>
-          <input
-            name="fullName"
-            value={inputs.fullName}
-            onChange={handleChange}
-            type="text" className="form-control" id="exampleInputFullName" placeholder='Full Name' required />
-        </div>)}
+      <form className=' me-5 w-50' onSubmit={sendData}>
 
-        <div className="mb-4 col-8">
-          <label for="exampleInputPassword1" className="form-label">Email</label>
-          <input
-            name="email"
-            value={inputs.email}
-            onChange={handleChange}
-            type="email" className="form-control" id="exampleInputEmail1" placeholder='Email' required />
+        <div className='row bg-danger'>
+
+
+          <div className='col-6 bg-success'> {
+             isLoginType ? <p>Soy el primer boton de la vista de login</p> :
+             <p>Soy el primer boton en la vista de registro</p>
+
+          } </div>
+          <div className='col-6 bg-secondary'>
+            {isLoginType ? <p>Soy el segundo boton de la vista de login</p> :
+            <p>Soy el segundo boton de la vista de registro</p>}
+          </div>
+
         </div>
 
-        <div className="mb-4 col-8">
-          <label for="exampleInputPassword1" className="form-label">Password</label>
+        {!isLoginType &&
+
+          (<>
+
+        
+        <div className="mb-4">
+          <label for="exampleInputName1" className="form-label">Nombre</label>
           <input
-            name="password"
-            value={inputs.password}
+            name="nombre"
+            value={inputs.nombre}
             onChange={handleChange}
-            type="password" className="form-control" id="exampleInputPassword1" placeholder='Password' required />
+            type="text" className="form-control rounded-pill" id="exampleInputName" placeholder='Nombre' required />
         </div>
 
-        <div className="d-grid gap-2 col-12 mx-auto mb-3">
-          <button type="submit" class="btn btn-success col-8">{isLoginType ? "Login" : "Sign up"}</button>
+        <div className="mb-4">
+          <label for="exampleInputApellidos1" className="form-label">Apellidos</label>
+          <input
+            name="apellidos"
+            value={inputs.apellidos}
+            onChange={handleChange}
+            type="text" className="form-control rounded-pill" id="exampleInputApellidos1" placeholder='Apellidos' required />
         </div>
 
-        {isLoginType ?
-          <Link to="/auth/signup">Create a new account</Link> :
-          <Link to="/auth/login">Already have an account?</Link>
-        }
+        <div className="mb-4">
+          <label for="exampleInputTelefono1" className="form-label">Teléfono</label>
+          <input
+            name="telefono"
+            value={inputs.telefono}
+            onChange={handleChange}
+            type="text" className="form-control rounded-pill" id="exampleInputTelefono1" placeholder='Teléfono' required />
+        </div>
+      </>
+          )}
 
-      </form>
 
-    </div>
+      <div className="mb-4">
+        <label for="exampleInputEmail1" className="form-label ">Email</label>
+        <input
+          name="email"
+          value={inputs.email}
+          onChange={handleChange}
+          type="email" className="form-control rounded-pill" id="exampleInputEmail1" placeholder='alguien@dominio.com' required />
+      </div>
+
+      <div className="mb-4">
+        <label for="exampleInputPassword1" className="form-label">Password</label>
+        <input
+          name="password"
+          value={inputs.password}
+          onChange={handleChange}
+          type="password" className="form-control rounded-pill" id="exampleInputPassword1" placeholder='**********' required />
+      </div>
+
+      <div className="d-grid gap-2 mb-3">
+        <button type="submit" className="btn btn-primary rounded-pill">{isLoginType ? "Iniciar sesión" : "Registrarse"}</button>
+      </div>
+
+      {isLoginType ?
+        <p>¿Todavía no tienes cuenta? <Link to="/auth/signup">Regístrate</Link></p> :
+
+        <p>¿Ya tienes cuenta? <Link to="/auth/login">Inicia Sesión</Link></p>
+      }
+
+    </form>
+
+    </div >
 
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,13 +23,12 @@ export const Home = () => {
 				<div className="row">
 					<section className="col-6 d-flex justify-content-center align-items-center flex-column">
 						<div className="col-12 ps-3">
-							<h1 className="text-start mb-3">{params.type == "login" ? "Login" : "Sign Up"}</h1>
-							<h6 className="text-start mb-3">Join top freelancers on Freelance</h6>
+							<h1 className="text-start mb-3">{params.type == "login" ? "Iniciar sesión" : "Registrarse"}</h1>
 						</div>
 						<RegisterForm isLoginType = {params.type == "login"}/>
 					</section>
 
-					<section className="col-6">
+					<section className="col-6 d-none d-md-block">
 						<img style={{ width: 500, height: "auto" }} src={coffeman} />
 					</section>
 
