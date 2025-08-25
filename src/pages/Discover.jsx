25db50eb0b4c7b37b13f4398432868f50eb901fe
Carlos Lorenzo <carloren.1996@gmail.com>
2025-08-25 11:26:18 +0000

    trabajando en la barra de filtros

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -66,15 +66,30 @@ export const Discover = () => {
                     value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
                 <button className="btn btn-dark btn-lg me-2 rounded-5" type="submit">Buscar</button>
             </form>
+            <div className="d-flex w-100 mb-3" role="search" onSubmit={handleSubmit}>
+                <div className="form-control me-2 rounded-5 text-bg-primary d-flex">
+                    <div className="d-flex align-items-center">
+                        <p className="my-0 text-nowrap">Filtros de búsqueda</p>
+                        <div className="ms-3 border border-light h-100"></div>
+                    </div>
+                    <div className="mx-auto h-100 d-flex align-items-center">
+                        <p className="my-0 mx-3 text-nowrap">Filtro 1</p>
+                        <p className="my-0 mx-3 text-nowrap">Filtro 2</p>
+                        <p className="my-0 mx-3 text-nowrap">Filtro 3</p>
+                        <p className="my-0 mx-3 text-nowrap">Filtro 4</p>
+                    </div>
+                </div>
+                <button className="btn btn-dark btn-lg me-2 rounded-5 text-nowrap">Reiniciar filtros</button>
+            </div>
             <div className="d-flex my-2">
-                <h3>Categorías</h3>
+                <h3 className="text-nowrap">Categorías</h3>
                 <div className="text-bg-dark ms-2 w-100 align-self-center" style={{ height: "1px" }}> </div>
             </div>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 pb-1 flex-nowrap hide-scroll overflow-auto">
                 {sortedCategories.map((cat) => <CategoryCard key={store.categories.indexOf(cat.category)} id={store.categories.indexOf(cat.category)} />)}
             </div>
             <div className="d-flex mb-2 mt-4">
-                <h3>Trabajadores</h3>
+                <h3 className="text-nowrap">Profesionales recomendados</h3>
                 <div className="text-bg-dark ms-2 w-100 align-self-center" style={{ height: "1px" }}> </div>
             </div>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 mb-5">
